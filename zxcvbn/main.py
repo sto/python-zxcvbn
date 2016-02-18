c4f63f2da8b21df7735645d0be6f51567e604000
sans-serif <noemailinside@gmail.com>
2016-02-18 12:15:46 +0100

    Moved to python3

--- zxcvbn/main.py
+++ zxcvbn/main.py
@@ -2,11 +2,12 @@ import time
 
 from zxcvbn.matching import omnimatch
 from zxcvbn.scoring import minimum_entropy_match_sequence
-
+from zxcvbn.feedback import get_feedback
 
 def password_strength(password, user_inputs=[]):
     start = time.time()
     matches = omnimatch(password, user_inputs)
     result = minimum_entropy_match_sequence(password, matches)
+    result.feedback = get_feedback(result.score, result.feedback)
     result['calc_time'] = time.time() - start
     return result
